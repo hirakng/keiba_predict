16484e3625ecc66627c17c682580d448f6d2c5df
hirakng <kng.hira@gmail.com>
2018-08-16 12:36:59 +0000

    umaban

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ html = urlopen(Url)
 bsObj = BeautifulSoup(html,"html.parser")
 
 #テーブルを指定
-table = bsObj.find('table', id="20180811")
+table = bsObj.find('table', id="20180818")
 tables = pd.read_html(Url)
 doyou =tables[3]
 #距離の１文字目と以降に分割
@@ -23,23 +23,26 @@ links = ["https://www.keibalab.jp"+x.get("href")+"umabashira.html"  for x in tab
 links = [s for s in links if 'race' in s]
 
 df_tan_nin = pd.DataFrame(columns=["単勝","人気"])
+if doyou['馬'].isnull()[0]==False:
+    for i in doyou.index:
+        html = urlopen(links[i])
+        bsObj = BeautifulSoup(html,"html.parser")
+        megamoriTable =bsObj.findAll("table",{"class":"megamoriTable"})[0]
+        rows = megamoriTable.findAll('tr', attrs={'class': "seirei std9"})
+        tds = rows[1].findAll('td')
+        td_no = 18 - doyou["馬"][i]
+        tan_ninki=tds[td_no].get_text().replace('\n','').replace('\t','').replace(' ','')
+        tansho = re.split('[()]', tan_ninki)[0]
+        ninki = re.split('[()]', tan_ninki)[1]
+        df_tan_nin = df_tan_nin.append(pd.DataFrame([[tansho, ninki]],columns=["単勝","人気"], index=[i]))
+        #print(tansho,",",ninki)
+        time.sleep(1)
 
-for i in doyou.index:
-    html = urlopen(links[i])
-    bsObj = BeautifulSoup(html,"html.parser")
-    megamoriTable =bsObj.findAll("table",{"class":"megamoriTable"})[0]
-    rows = megamoriTable.findAll('tr', attrs={'class': "seirei std9"})
-    tds = rows[1].findAll('td')
-    td_no = 18 - doyou["馬"][i]
-    tan_ninki=tds[td_no].get_text().replace('\n','').replace('\t','').replace(' ','')
-    tansho = re.split('[()]', tan_ninki)[0]
-    ninki = re.split('[()]', tan_ninki)[1]
-    df_tan_nin = df_tan_nin.append(pd.DataFrame([[tansho, ninki]],columns=["単勝","人気"], index=[i]))
-    #print(tansho,",",ninki)
-    time.sleep(1)
-
-slack = slackweb.Slack(url=os.environ.get('WEBHOOK_URL'))
-slack.notify(text="条件に合致するレースを報告します")    
-for i in doyou.index:
-    slack.notify(text="第"+str(doyou['R'][i])+"レース"+str(doyou['レース名'][i])+"が"+df_tan_nin["人気"][i]+"番人気で、"
-                 +"単勝は"+df_tan_nin["単勝"][i]+"です")
+    slack = slackweb.Slack(url=os.environ.get('WEBHOOK_URL'))
+    slack.notify(text="条件に合致するレースを報告します")    
+    for i in doyou.index:
+        slack.notify(text="第"+str(doyou['R'][i])+"レース"+str(doyou['レース名'][i])+"が"+df_tan_nin["人気"][i]+"番人気で、"
+                    +"単勝は"+df_tan_nin["単勝"][i]+"です")
+else:
+    slack = slackweb.Slack(url=os.environ.get('WEBHOOK_URL'))
+    slack.notify(text="馬番決定前です")    
